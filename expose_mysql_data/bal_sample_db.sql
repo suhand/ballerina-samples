a1627ac0f05ae40fab21911d3dfcab9626ccacf5
suhand <suhand85@gmail.com>
2021-03-30 11:59:44 +0530

    DB script updated to support service sample

--- expose_mysql_data/bal_sample_db.sql
+++ expose_mysql_data/bal_sample_db.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS bal_sample_db;
+
 CREATE DATABASE bal_sample_db;
 
 USE bal_sample_db;
@@ -6,10 +8,14 @@ CREATE TABLE person (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    department VARCHAR(50) NOT NULL,
     email VARCHAR(50),
     registered_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `email`) VALUES ('Star', 'Load', 'star.load@gmail.com');
-INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `email`) VALUES ('Peter', 'Parker', 'p.parker@gmail.com');
-INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `email`) VALUES ('Tony', 'Stark', 'tony@gmail.com');
+INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `department`, `email`) VALUES ('Tony', 'Stark', 'Avengers', 'tony@starkindustries.com');
+INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `department`, `email`) VALUES ('Star', 'Lord', 'Avengers', 's.lord@avengers.com');
+INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `department`, `email`) VALUES ('Peter', 'Parker', 'Avengers', 'p.parker@gmail.com');
+INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `department`, `email`) VALUES ('Chandler', 'Bing', 'Friends', 'c.bing@friends.com');
+INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `department`, `email`) VALUES ('Monica', 'Geller', 'Friends', 'monica@friends.com');
+INSERT INTO `bal_sample_db`.`person` (`first_name`, `last_name`, `department`, `email`) VALUES ('Joey', 'Tribbiani', 'Friends', 'joey@friends.com');
